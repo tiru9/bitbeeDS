f052aba1407801aded794382ac5231c88e65f207
tiru9 <java.tirupati@yahoo.com>
2024-02-17 12:17:25 +0530

    Added sorting implementation code for bubble and selection sort

--- python/sorting_algos/SelectionSort/bubble_sort.py
+++ python/sorting_algos/SelectionSort/bubble_sort.py
@@ -10,7 +10,8 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 # Test the bubble_sort function
-arr = [64, 34, 25, 12, 22, 11, 90]
+arr = [20, 30, 50, 40, 10]
 print("Original array:", arr)
 bubble_sort(arr)
 print("Sorted array:", arr)
+
