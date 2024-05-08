efea930a55c749b6145355d0197053aae4690de4
Bitbee <53965480+tiru9@users.noreply.github.com>
2024-05-08 08:01:28 +0530

    Update InsertionSort.java

---
+++ src/java/sorting_algos/InsertionSort.java
@@ -0,0 +1,36 @@
+public class InsertionSort {
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1], that are greater than key, to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6};
+        
+        System.out.println("Original array:");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("\nSorted array:");
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
